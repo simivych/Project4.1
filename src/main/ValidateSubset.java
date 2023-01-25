83682ec147885c625c5249dbd2627d6208bfb9d1
simivych <56159776+simivych@users.noreply.github.com>
2023-01-25 03:03:00 +0100

    subset fixes

--- src/main/ValidateSubset.java
+++ src/main/ValidateSubset.java
@@ -31,7 +31,7 @@ public class ValidateSubset {
     static ArrayList<Double> subset12 = new ArrayList<>();
 
     static String[] results = new String[]{"results/resultsAlphaBeta.txt", "results/resultsUCT.txt", "results/resultsPNMCTS.txt",
-            "results/resultsGRAVE.txt", "results/resultsPortML"};
+            "results/resultsGRAVE.txt", "results/resultsPortMLTime.txt"};
 
     static String[] subsets = new String[]{"subsets/Birch/subset_5_filtered.csv","subsets/Birch/subset_5.csv",
             "subsets/Birch/subset_5_filtered_PCA.csv","subsets/Birch/subset_5_PCA.csv", "subsets/K_means/subset_5.csv",
